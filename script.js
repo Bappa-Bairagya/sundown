506bf70c083e3b10074b40fa561c9b99d1d62c6c
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-03-25 20:53:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+const elemC = document.querySelector(".elem-container")
+const fixed = document.querySelector("#fixed")
+elemC.addEventListener("mouseenter", function () {
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none"
+})
+const elems = document.querySelectorAll(".elem")
+
+elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+        var data = e.getAttribute("data")
+        console.log(data);
+
+        fixed.style.backgroundImage = `url(${data})`
+
+    })
+
+})
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var loader = document.querySelector("#loader"); // Selects the element with id "loader"
+
+setTimeout(function() {
+    loader.style.top = "-100%"; // Moves the loader up by 100% (presumably out of view)
+}, 4000); // Executes after 2000 milliseconds (2 seconds)
+
