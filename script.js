19cc7f265bed267a1ed0ace6210cf1b97b0e1736
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-04-07 13:42:59 +0530

    practice

--- script.js
+++ script.js
@@ -33,9 +33,24 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-var loader = document.querySelector("#loader"); // Selects the element with id "loader"
+// var loader = document.querySelector("#loader"); // Selects the element with id "loader"
 
-setTimeout(function() {
-    loader.style.top = "-100%"; // Moves the loader up by 100% (presumably out of view)
-}, 4000); // Executes after 2000 milliseconds (2 seconds)
-
+// setTimeout(function() {
+//     loader.style.top = "-100%"; // Moves the loader up by 100% (presumably out of view)
+// }, 4000); // Executes after 2000 milliseconds (2 seconds)
+var slid=document.querySelector("#slid")
+var menu=document.querySelector("nav h3")
+var navimg=document.querySelector("nav img")
+var flag=0
+menu.addEventListener("click",()=>{
+    if (flag==0) {
+        slid.style.top=0
+    navimg.style.opacity=0
+    flag=1
+    } else {
+        slid.style.top="-100%"
+        navimg.style.opacity=1
+        flag=0
+    }
+    
+})
